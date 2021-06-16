4d3bc888485e955883f5094856a59807f2241678
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-06-15 21:19:58 -0400

    Suppress namespaces

--- analysis/AssemblyCodeAnalysisSuppresions.cs
+++ analysis/AssemblyCodeAnalysisSuppresions.cs
@@ -12,34 +12,38 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage(
 	"Microsoft.Naming",
 	"CA1720:IdentifiersShouldNotContainTypeNames",
-	Justification = "Developers should know the difference between a builtin type and an identifier on a type with the same name as a builtin type.")]
+	Justification = "Developers should know the difference between a builtin type and an identifier on a type with the same name.")]
 
 [assembly: SuppressMessage(
 	"Microsoft.Naming",
 	"CA1711:IdentifiersShouldNotHaveIncorrectSuffix",
-	Justification = "Naming of types with a certain suffix like 'Exception' or 'Collection' should not reserved just for Microsoft; developers should be free to name types as they wish within reason.")]
+	Justification = "Naming of types with a certain suffix like 'Exception' or 'Collection' should not reserved just for Microsoft; developers should be free to name types as they wish.")]
 
 [assembly: SuppressMessage(
 	"Microsoft.Naming",
 	"CA1822:MarkMembersAsStatic",
-	Justification = "If something could be static but it's not then the developer made a conscience choice.")]
+	Justification = "If something could be static but it's not then the developer made a and deliberate choice.")]
 	
 [assembly: SuppressMessage(
 	"ReSharper",
 	"MemberCanBeMadeStatic.Local",
-	Justification = "If something could be static but it's not then the developer made a conscience choice.")]
+	Justification = "If something could be static but it's not then the developer made a conscience and deliberate choice.")]
 
 [assembly: SuppressMessage(
 	"ReSharper",
 	"MemberCanBeMadeStatic.Global",
-	Justification = "If something could be static but it's not then the developer made a conscience choice.")]
+	Justification = "If something could be static but it's not then the developer made a conscience and deliberate choice.")]
 	
 [assembly: SuppressMessage(
 	"Microsoft.Naming",
 	"CA1716:IdentifiersShouldNotMatchKeywords",
-	Justification = "If a developer named something that matches an identifier it's a conscience choice.")]
+	Justification = "If a developer named something that matches an identifier it's a conscience and deliberate choice.")]
 	
 [assembly: SuppressMessage(
 	"Microsoft.Naming",
 	"CA1707:IdentifiersShouldNotContainUnderscores",
 	Justification = "Underscores are permissable due to dealing with C transpiled code.")]
+	
+[assembly: SuppressMessage("Microsoft.Design", 
+	"CA1050:DeclareTypesInNamespaces", 
+	Justification = "To namespace or not to namespace is a developer descision on a case-by-case basis; there should be no blanket rule that says all folders or containers of organization for code should have their own namespace.")]
