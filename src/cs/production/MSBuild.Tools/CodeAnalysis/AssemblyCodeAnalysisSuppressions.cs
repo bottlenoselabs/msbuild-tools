76cfd85fcea4436760a02815bb2d5ee289526728
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-12-07 01:09:21 -0500

    Suppress `CA5392`

--- src/cs/production/MSBuild.Tools/CodeAnalysis/AssemblyCodeAnalysisSuppressions.cs
+++ src/cs/production/MSBuild.Tools/CodeAnalysis/AssemblyCodeAnalysisSuppressions.cs
@@ -5,27 +5,27 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage(
-	"Microsoft.Naming",
+	"Microsoft Naming",
 	"CA1062:ValidateArgumentsOfPublicMethods",
 	Justification = "Always use C# nullables which makes this rule redundant and noisy.")]
 
 [assembly: SuppressMessage(
-	"Microsoft.Naming",
+	"Microsoft Naming",
 	"CA1051:DoNotDeclareVisibleInstanceFields",
 	Justification = "'fields should be as an implementation detail' is wrong; fields are data and developers need to work with data.")]
 
 [assembly: SuppressMessage(
-	"Microsoft.Naming",
+	"Microsoft Naming",
 	"CA1720:IdentifiersShouldNotContainTypeNames",
 	Justification = "Developers should know the difference between a builtin type and an identifier on a type with the same name.")]
 
 [assembly: SuppressMessage(
-	"Microsoft.Naming",
+	"Microsoft Naming",
 	"CA1711:IdentifiersShouldNotHaveIncorrectSuffix",
 	Justification = "Naming of types with a certain suffix like 'Exception' or 'Collection' should not reserved just for Microsoft; developers should be free to name types as they wish.")]
 
 [assembly: SuppressMessage(
-	"Microsoft.Naming",
+	"Microsoft Naming",
 	"CA1822:MarkMembersAsStatic",
 	Justification = "If something could be static but it's not then the developer made a and deliberate choice.")]
 	
@@ -40,15 +40,23 @@ using System.Diagnostics.CodeAnalysis;
 	Justification = "If something could be static but it's not then the developer made a conscience and deliberate choice.")]
 	
 [assembly: SuppressMessage(
-	"Microsoft.Naming",
+	"Microsoft Naming",
 	"CA1716:IdentifiersShouldNotMatchKeywords",
 	Justification = "If a developer named something that matches an identifier it's a conscience and deliberate choice.")]
 	
 [assembly: SuppressMessage(
-	"Microsoft.Naming",
+	"Microsoft Naming",
 	"CA1707:IdentifiersShouldNotContainUnderscores",
 	Justification = "Underscores are permissable due to dealing with C transpiled code.")]
 	
-[assembly: SuppressMessage("Microsoft.Design", 
+[assembly: SuppressMessage(
+	"Microsoft Design", 
 	"CA1050:DeclareTypesInNamespaces", 
 	Justification = "To namespace or not to namespace is a developer descision on a case-by-case basis; there should be no blanket rule that says all folders or containers of organization for code should have their own namespace.")]
+
+[assembly: SuppressMessage(
+	"Microsoft Security",
+	"CA5392",
+	MessageId = "Use DefaultDllImportSearchPaths attribute for P/Invokes.",
+	Justification = "Always ensure the full path is specified, see: https://msrc-blog.microsoft.com/2014/05/13/load-library-safely/. Also, this warning only applies to Windows but macOS and Linux see the thing still, quite annoying.")]
+	
